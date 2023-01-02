4ca70c63e8d59dc9a532b3b754605305b160c8d8
Fx Gt <102917651+fxgt@users.noreply.github.com>
2023-01-02 15:30:22 -0500

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,4 +1,4 @@
 # Change these variables values with your informations
 identifiant = '<Add your code permanent>'
 motDePasse = '<Add your nip>'
-email_recever = '<Add your email>'
+email_recever = '<Add your email that will receive the notification>'

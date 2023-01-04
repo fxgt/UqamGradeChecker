237942bb780880703c905afec029acc842fdca8e
Fx Gt <102917651+fxgt@users.noreply.github.com>
2023-01-03 19:58:29 -0500

    Update creds.py

--- creds.py
+++ creds.py
@@ -2,3 +2,8 @@
 identifiant = '<Add your code permanent>'
 motDePasse = '<Add your nip>'
 email_recever = '<Add your email that will receive the notification>'
+
+#Create Twilio account to fill the next variables
+sms_sender_number='<your Twilio number>'
+account_sid = '<Your Twilio account sid>'
+auth_token = '<Your Twilio authentification Token>'
